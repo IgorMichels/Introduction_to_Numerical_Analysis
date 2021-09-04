fca3060a510d365d52fc8bbebfb4691fb6599c43
IgorMichels <igorxadrezbr@gmail.com>
2021-09-04 14:47:20 -0300

    execute.py

---
+++ Lists/Lista 2/exe.py
@@ -0,0 +1,51 @@
+from tabulate import tabulate
+from create_system import *
+from sor import *
+import math
+
+# finding optmal omega
+omega_opt = lambda m : 1 / (1 + math.sin(np.pi / (m + 1)))
+
+tol = 10e-6
+ms = [50, 100, 1000, 5000]
+omegas = [omega_opt, 1, .5, 0]
+n_iters = [[ms[i]] + list(np.zeros(len(omegas), int)) for i in range(len(ms))]
+
+for i in range(len(ms)):
+	m = ms[i]
+	for j in range(len(omegas)):
+		omega = omegas[j]
+		if omega == omega_opt:
+			omega = omega_opt(m)
+			
+		A = create_A(m)
+		b = create_b(m)
+		x0 = np.zeros((A.shape[0], 1))
+		x, error, n_iter = sor(A, b, omega, x0, tol, np.inf)
+		n_iters[i][j + 1] = n_iter
+		
+print(tabulate(n_iters,
+			   headers = ['m\ω', 'ω ótimo', 1, 0.5, 0],
+			   tablefmt = 'fancy_grid'))
+		
+print(tabulate(n_iters,
+			   headers = ['m\ω', 'ω ótimo', 1, 0.5, 0],
+			   tablefmt = 'latex'))
+
+
+
+
+'''
+omega = 1
+tol = 10e-6
+norm = 'inf'
+A = create_A(m)
+b = create_b(m)
+x0 = np.zeros((A.shape[0], 1))
+
+x, error, n_iter = sor(A, b, omega, x0, tol, np.inf)
+print(x)
+print(n_iter)
+
+print('ω')
+'''
