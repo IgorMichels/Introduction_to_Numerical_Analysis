3b09056a4fbea2717f95bc509571362900f9523f
IgorMichels <igorxadrezbr@gmail.com>
2021-09-04 18:08:31 -0300

    some changes

--- Lists/Lista 2/exe.py
+++ Lists/Lista 2/exe.py
@@ -4,32 +4,43 @@ from sor import *
 import math
 
 # finding optmal omega
-omega_opt = lambda m : 1 / (1 + math.sin(np.pi / (m + 1)))
+omega_opt = lambda m : 2 / (1 + math.sin(np.pi / (m + 1)))
 
 tol = 10e-6
 ms = [50, 100, 1000, 5000]
-omegas = [omega_opt, 1, .5, 0]
+omegas = [omega_opt, 1, .5]
 n_iters = [[ms[i]] + list(np.zeros(len(omegas), int)) for i in range(len(ms))]
+errors = [[ms[i]] + list(np.zeros(len(omegas), int)) for i in range(len(ms))]
+solutions = [[ms[i]] + list(np.zeros(len(omegas), int)) for i in range(len(ms))]
 
 for i in range(len(ms)):
 	m = ms[i]
+	A = create_A(m)
+	b = create_b(m)
+	x0 = np.random.normal(2, .0001, A.shape[0])
 	for j in range(len(omegas)):
 		omega = omegas[j]
 		if omega == omega_opt:
 			omega = omega_opt(m)
-			
-		A = create_A(m)
-		b = create_b(m)
-		x0 = np.zeros((A.shape[0], 1))
-		x, error, n_iter = sor(A, b, omega, x0, tol, np.inf)
+		
+		x0_j = x0.copy()
+		x, error, n_iter = sor(A, b, omega, x0_j, tol, np.inf)
 		n_iters[i][j + 1] = n_iter
 		
 print(tabulate(n_iters,
-			   headers = ['m\ω', 'ω ótimo', 1, 0.5, 0],
+			   headers = ['m\ω', 'ω ótimo', 1, 0.5],
 			   tablefmt = 'fancy_grid'))
 		
 print(tabulate(n_iters,
-			   headers = ['m\ω', 'ω ótimo', 1, 0.5, 0],
+			   headers = ['m\ω', 'ω ótimo', 1, 0.5],
+			   tablefmt = 'latex'))
+		
+print(tabulate(errors,
+			   headers = ['m\ω', 'ω ótimo', 1, 0.5],
+			   tablefmt = 'fancy_grid'))
+		
+print(tabulate(errors,
+			   headers = ['m\ω', 'ω ótimo', 1, 0.5],
 			   tablefmt = 'latex'))
 
 
--- Lists/Lista 2/sor.py
+++ Lists/Lista 2/sor.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def sor(A, b, omega, x0, tol, norm = np.inf, max_iter = 10000):
+def sor(A, b, omega, x0, tol, norm = np.inf, max_iter = 10e4):
 	'''
 	solves Ax = b system with SOR method with w = omega
 	and tolerance tol in the given norm 
@@ -11,17 +11,17 @@ def sor(A, b, omega, x0, tol, norm = np.inf, max_iter = 10000):
 	elif type(norm) == int:
 		norm = lambda x : np.linalg.norm(x, norm)
 	
-	x = x0
+	x = x0.copy()
 	error = norm(A @ x - b)
 	n_iter = 0
 	while error > tol and n_iter < max_iter:
 		for i in range(A.shape[0]):
-			sigma = 0
+			mult = 0
 			for j in range(A.shape[1]):
 				if j != i:
-					sigma += A[i, j] * x0[j]
+					mult += A[i, j] * x[j]
 			
-			x[i] = (1 - omega) * x[i] + omega / A[i, i] * (b[i] - sigma)
+			x[i] = (1 - omega) * x[i] + omega / A[i, i] * (b[i] - mult)
 		
 		error = norm(A @ x - b)
 		n_iter += 1
