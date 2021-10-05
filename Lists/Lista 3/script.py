404f9c490f42207d6988f3886002feda2c80074b
IgorMichels <igorxadrezbr@gmail.com>
2021-10-05 10:59:37 -0300

    update lista 3

--- Lists/Lista 3/script.py
+++ Lists/Lista 3/script.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.optimize import minimize
 
 def f(x):
 	return np.log(15 - np.log(x)) - x
@@ -6,29 +7,50 @@ def f(x):
 def f_prime(x):
 	return - (1 / (x*(15 - np.log(x)))) - 1
 	
+def f_prime_abs(x):
+	return abs(f_prime(x))
+	
+def f_prime_prime(x):
+	return (14 - np.log(x)) / (x**2 * (15 - np.log(x))**2)
+	
+def f_prime_prime_abs(x):
+	return - abs(f_prime_prime(x))
+	
 def g(x):
 	return np.log(15 - np.log(x))
 	
+def g_prime(x):
+	return - 1 / (x * (15 - np.log(x)))
+	
+def g_prime_abs(x):
+	return - abs(- 1 / (x * (15 - np.log(x))))
+	
 def fix_point(x0, tol):
 	x = x0
-	dif = abs(f(x))
+	k = minimize(g_prime_abs, 2, bounds = [(1, 3)])
+	k = - k.fun
+	dif = tol + 1
 	i = 0
 	while dif > tol:
 		x0 = x
 		x = g(x0)
-		dif = abs(f(x))
+		dif = k / (1 - k) * abs(x - x0)
 		i += 1
 		
 	return x, i
 	
 def newton_raphson(x0, tol):
 	x = x0
-	dif = abs(f(x))
+	M = minimize(f_prime_prime_abs, 2, bounds = [(1, 3)])
+	m = minimize(f_prime_abs, 2, bounds = [(1, 3)])
+	M = - M.fun
+	m = m.fun
+	dif = tol + 1
 	i = 0
 	while dif > tol:
 		x0 = x
 		x = x0 - f(x0)/f_prime(x0)
-		dif = abs(f(x))
+		dif = M / (2 * m) * abs(x - x0) ** 2
 		i += 1
 		
 	return x, i
